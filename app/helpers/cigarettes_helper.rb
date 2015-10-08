47d7246698e3c6838f4b48cfd1fa6c53f3e98ea7
Benoit Liabastre <b.liabastre@gmail.com>
2015-10-08 17:31:44 +0200

    ui

--- app/helpers/cigarettes_helper.rb
+++ app/helpers/cigarettes_helper.rb
@@ -4,17 +4,20 @@ module CigarettesHelper
     @cigarettes = Cigarette.all
     @cigarettes_by_day = []
     @first_cigarette = Cigarette.order(date: :asc).first
-    tmp_date = Date.tomorrow
-    begin_date = @first_cigarette.present? ? @first_cigarette.date.to_date : Date.parse('2015-09-08')
+    tmp_date = Date.today
+    begin_date = @first_cigarette.present? ? @first_cigarette.date.to_date : nil
     puts tmp_date.inspect
     puts begin_date.inspect
-    (begin_date..tmp_date).each do |day|
-      puts day.to_date.inspect
-      @cigarettes_by_day << {
-        date: day.to_date,
-        cigarettes: Cigarette.where(date: (day.beginning_of_day..day.end_of_day)).to_a
-      }
+    if begin_date
+      (tmp_date..begin_date).each do |day|
+        puts day.to_date.inspect
+        @cigarettes_by_day << {
+          date: day.to_date,
+          cigarettes: Cigarette.where(date: (day.beginning_of_day..day.end_of_day)).to_a
+        }
+      end
     end
+    30.times{ @cigarettes_by_day << @cigarettes_by_day[0] }
     puts @cigarettes_by_day.inspect
   end
 end
